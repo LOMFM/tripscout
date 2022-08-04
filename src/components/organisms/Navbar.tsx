4f6ac6767b0c44df5cabf045b6e908a9de4d17fb
xiao <brightdawn019@gmail.com>
2022-08-03 22:58:13 -0400

    feat(i18n): insert the i18n with English and Spanish

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import { SearchBox } from "../molecules/SearchBox";
 
+enum LANG {
+    EN = 'en',
+    ES = 'es'
+}
+
 const NavBar: React.FC = () => {
+    let navigate = useNavigate();
+    const { t, i18n } = useTranslation();
+    const [lang, setLang] = useState(LANG.EN);
+    
     const onSearch = (v: string) => {
+        navigate(v ? '/destination/' + v : '/');
     }
+
+    const changeLang = () => {
+        const newLang = lang === LANG.EN ? LANG.ES : LANG.EN;
+        setLang(newLang)
+        i18n.changeLanguage(newLang);
+    };
 
     return (
         <div className="navbar">
             <div className="logo"></div>
-            <SearchBox onSearch={onSearch} placeholder={'search'} />
+            <SearchBox onSearch={onSearch} placeholder={t('search')} />
+            <button onClick={changeLang} className="lang-btn">
+                { lang === LANG.EN ? 'Española' : 'English' }
+            </button>
         </div>
     )
 }
